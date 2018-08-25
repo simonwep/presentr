489add877349d7e2261f145cf3192d765d95719c
Simonwep <toports@gmx.de>
2018-08-25 09:00:48 +0200

    Add prefix to private attributes

--- src/presentr.js
+++ src/presentr.js
@@ -36,21 +36,21 @@ function Presentr(opt = {}) {
             }, opt);
 
             // Slides
-            that.slideIndex = 0;
-            that.slides = _.selectAll(that.options.slides);
-            that.presentrSlides = that.slides[0].parentElement;
-            that.presentrRoot = that.presentrSlides.parentElement;
+            that._slideIndex = 0;
+            that._slides = _.selectAll(that.options.slides);
+            that._presentrSlides = that._slides[0].parentElement;
+            that._presentrRoot = that._presentrSlides.parentElement;
 
             // Fragments
-            that.fragmentIndex = 0;
-            that.fragments = that.slides.map(s => _.selectAll(that.options.fragments, s));
+            that._fragmentIndex = 0;
+            that._fragments = that._slides.map(s => _.selectAll(that.options.fragments, s));
 
             // Components
-            that.progress = document.querySelector(that.options.progress);
+            that._progress = document.querySelector(that.options.progress);
 
             // Inject styles
-            _.css(that.presentrRoot, {'overflow': 'hidden'});
-            _.css(that.presentrSlides, {
+            _.css(that._presentrRoot, {'overflow': 'hidden'});
+            _.css(that._presentrSlides, {
                 'position': 'fixed',
                 'display': 'flex',
                 'top': '0',
@@ -97,42 +97,42 @@ function Presentr(opt = {}) {
             target && fns.includes(target) && that[target]();
         },
 
-        nextSlide: () => that.jumpSlide(that.slideIndex + 1),
-        previousSlide: () => that.jumpSlide(that.slideIndex - 1),
+        nextSlide: () => that.jumpSlide(that._slideIndex + 1),
+        previousSlide: () => that.jumpSlide(that._slideIndex - 1),
         jumpSlide(index) {
 
             // Validate
-            if (index < 0 || index >= that.slides.length) {
+            if (index < 0 || index >= that._slides.length) {
                 return false;
             }
 
             // Fire event
-            that.options.onSlide(that, index, that.slides.length - 1);
+            that.options.onSlide(that, index, that._slides.length - 1);
 
             // Apply index
-            that.slideIndex = index;
+            that._slideIndex = index;
 
             // Update offset
-            that.presentrSlides.style.left = `-${index * 100}vw`;
+            that._presentrSlides.style.left = `-${index * 100}vw`;
 
             // Set active class
-            for (let i = 0, cl, n = that.slides.length; i < n && (cl = that.slides[i].classList); i++) {
+            for (let i = 0, cl, n = that._slides.length; i < n && (cl = that._slides[i].classList); i++) {
                 cl[i <= index ? 'add' : 'remove'](that.options.activeSlideClass);
                 cl[i === index ? 'add' : 'remove'](that.options.currentSlideClass);
             }
 
             // Update fragment index
-            that.fragmentIndex = that.fragments[index].reduce((ac, cv, ci) => cv.classList.contains(that.options.activeFragmentClass) ? ci + 1 : ac, 0);
+            that._fragmentIndex = that._fragments[index].reduce((ac, cv, ci) => cv.classList.contains(that.options.activeFragmentClass) ? ci + 1 : ac, 0);
 
-            // Update progress
-            that.progress.style.width = `${(index / (that.slides.length - 1)) * 100}%`;
+            // Update _progress
+            that._progress.style.width = `${(index / (that._slides.length - 1)) * 100}%`;
             return true;
         },
 
-        nextFragment: () => that.jumpFragment(that.fragmentIndex + 1),
-        previousFragment: () => that.jumpFragment(that.fragmentIndex - 1),
+        nextFragment: () => that.jumpFragment(that._fragmentIndex + 1),
+        previousFragment: () => that.jumpFragment(that._fragmentIndex - 1),
         jumpFragment(index) {
-            const fragments = that.fragments[that.slideIndex];
+            const fragments = that._fragments[that._slideIndex];
 
             // Jump to next / previous slide if no further fragments
             if (index < 0) {
@@ -145,7 +145,7 @@ function Presentr(opt = {}) {
             that.options.onFragment(that, index, fragments.length - 1);
 
             // Apply index
-            that.fragmentIndex = index;
+            that._fragmentIndex = index;
             for (let i = 0, cl, n = fragments.length; i < n && (cl = fragments[i].classList); i++) {
                 cl[i < index ? 'add' : 'remove'](that.options.activeFragmentClass);
                 cl[i === index - 1 ? 'add' : 'remove'](that.options.currentFragmentClass);
