5216b61da113dde9fd372449d5e08652f3f094ee
Simonwep <toports@gmx.de>
2018-08-30 21:05:11 +0200

    Update to babel 7

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    presets: [
+        [
+            '@babel/preset-env', {
+                'targets': {
+                    'browsers': [
+                        'Chrome >= 52',
+                        'FireFox >= 45',
+                        'Safari >= 10',
+                        'edge 15'
+                    ]
+                },
+                'modules': 'umd'
+            }
+        ]
+    ]
+
+};
