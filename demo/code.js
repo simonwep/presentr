a58eed163465e7a9ebeb574beeaf95dee9a40c71
Simonwep <toports@gmx.de>
2018-08-31 21:48:48 +0200

    Add demo page

--- demo/code.js
+++ demo/code.js
@@ -1,6 +1,13 @@
 const processbar = document.querySelector('.progress');
+
 const presentr = new Presentr({
-    onSlide(opt) {
+    onAction(opt) {
         processbar.style.width = `${(100 * opt.slidePercent)}vw`;
     }
+});
+
+window.addEventListener('touchstart', e => {
+    const x = e && e.touches && e.touches[0] && e.touches[0].clientX;
+    const halfWidth = window.innerWidth / 2;
+    x > halfWidth ? presentr.nextFragment() : presentr.previousFragment();
 });
